d304eca1508e81953989318d196a3c8694c9eecf
elton-cs <govada.elton@gmail.com>
2024-06-14 11:51:09 -0500

    Add basic model retrieval example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use starknet_ff::FieldElement;
 use torii_client::client::Client;
 use torii_grpc::types::KeysClause;
@@ -13,9 +15,23 @@ async fn main() {
         "0xb4079627ebab1cd3cf9fd075dda1ad2454a7a448bf659591f259efa2519b18",
     )
     .unwrap();
-    let models_keys = None;
 
-    let client = Client::new(torii_url, rpc_url, relay_url, world, models_keys)
+    let client = Client::new(torii_url, rpc_url, relay_url, world, None)
         .await
         .unwrap();
+
+    let position_key = FieldElement::from_hex_be(
+        "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
+    )
+    .unwrap();
+    let vec_keys = vec![position_key];
+
+    let position_key_clause = KeysClause {
+        model: "Position".to_string(),
+        keys: vec_keys,
+    };
+
+    if let Some(data) = client.model(&position_key_clause).await.unwrap() {
+        println!("POSITION MODEL: {:?}", data);
+    }
 }
