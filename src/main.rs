87965ce7eb24bea112f43f4afcd1a30d258c8037
elton-cs <govada.elton@gmail.com>
2024-06-14 21:00:41 -0500

    Clean, document, and refactor stream subscription example

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use starknet_crypto::poseidon_hash_many;
 use starknet_ff::FieldElement;
 use tokio_stream::StreamExt;
 use torii_client::client::Client;
-use torii_grpc::types::{schema::Entity, Clause, KeysClause, Query};
 
 #[tokio::main]
 async fn main() {
     println!("Running a rust torii client!");
 
+    // client configuration
     let torii_url = "http://0.0.0.0:8080".to_string();
     let rpc_url = "http://0.0.0.0:5050".to_string();
     let relay_url = "/ip4/127.0.0.1/tcp/9090".to_string();
@@ -18,59 +18,27 @@ async fn main() {
     )
     .unwrap();
 
+    // create a new client
     let client: Client = Client::new(torii_url, rpc_url, relay_url, world, None)
         .await
         .unwrap();
 
-    // example 1: manually calling the torii server to get a model's data
-    // let position_key_clause = build_position_key_clause();
-    // if let Some(data) = client.model(&position_key_clause).await.unwrap() {
-    //     println!("POSITION MODEL: {:?}", data);
-    // }
-
-    // example 2: Querying the torii server for a list of entities
-    // let query = Query {
-    //     // clause: None,
-    //     clause: Some(Clause::Keys(build_position_key_clause())),
-    //     limit: 100,
-    //     offset: 0,
-    // };
-    // let entities = client.entities(query).await.unwrap();
-    // for entity in entities {
-    //     println!("ENTITY: {:?}", entity);
-    // }
-
-    // let subscription = client.start_subscription().await.unwrap();
-    // subscription.await;
-
+    // create hash of all models' keys (in this case, just one: the player's contract addresss)
     let player_contract_address = FieldElement::from_hex_be(
         "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
     )
     .unwrap();
     let vec_keys = vec![player_contract_address.clone()];
-
     let hashed_keys = poseidon_hash_many(&vec_keys);
+
+    // subscribe to the player's contract address
     let mut stream = client.on_entity_updated(vec![hashed_keys]).await.unwrap();
 
+    // listen for incoming data
     loop {
         if let Some(data) = stream.next().await {
             println!("Incoming Data: {:?}", data);
             println!("----------");
         }
     }
-}
-
-fn build_position_key_clause() -> KeysClause {
-    let position_key = FieldElement::from_hex_be(
-        "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
-    )
-    .unwrap();
-    let vec_keys = vec![position_key];
-
-    let position_key_clause = KeysClause {
-        model: "Moves".to_string(),
-        keys: vec_keys,
-    };
-
-    position_key_clause
 }
