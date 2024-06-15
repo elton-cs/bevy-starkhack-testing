8d522fc2890a3be3a15898c0c4ff013bd38b9813
elton-cs <govada.elton@gmail.com>
2024-06-14 22:40:32 -0500

    Implement function wrapper for events stream (wip)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use starknet_crypto::poseidon_hash_many;
 use starknet_ff::FieldElement;
 use tokio_stream::StreamExt;
 use torii_client::client::Client;
+use torii_grpc::client::EntityUpdateStreaming;
 
 #[tokio::main]
 async fn main() {
@@ -23,6 +24,15 @@ async fn main() {
         .await
         .unwrap();
 
+    let mut stream = get_entities_stream(&client).await;
+
+    while let Some(data) = stream.next().await {
+        println!("Incoming Data: {:?}", data);
+        println!("----------");
+    }
+}
+
+async fn get_entities_stream(client: &Client) -> EntityUpdateStreaming {
     // create hash of all models' keys (in this case, just one: the player's contract addresss)
     let player_contract_address = FieldElement::from_hex_be(
         "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
@@ -32,13 +42,26 @@ async fn main() {
     let hashed_keys = poseidon_hash_many(&vec_keys);
 
     // subscribe to the player's contract address
-    let mut stream = client.on_entity_updated(vec![hashed_keys]).await.unwrap();
+    let stream = client.on_entity_updated(vec![hashed_keys]).await.unwrap();
 
-    // listen for incoming data
-    loop {
-        if let Some(data) = stream.next().await {
-            println!("Incoming Data: {:?}", data);
-            println!("----------");
-        }
-    }
+    stream
+}
+
+// todo: find what the event_key represents
+async fn _get_events_stream(client: &Client) -> EntityUpdateStreaming {
+    // create hash of all models' keys (in this case, just one: the player's contract addresss)
+    let event_key = FieldElement::from_hex_be(
+        "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
+    )
+    .unwrap();
+    let vec_keys = vec![event_key.clone()];
+    let hashed_keys = poseidon_hash_many(&vec_keys);
+
+    // subscribe to the player's contract address
+    let stream = client
+        .on_event_message_updated(vec![hashed_keys])
+        .await
+        .unwrap();
+
+    stream
 }
