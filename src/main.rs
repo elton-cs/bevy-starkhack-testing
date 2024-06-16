a92b1954575b928bdf502b20b3a1a79a51e879b0
elton-cs <govada.elton@gmail.com>
2024-06-15 20:05:32 -0500

    Add basic 2d render bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,102 @@
-use bevy_starkhack_testing::handling::get_current_position;
-use starknet_crypto::poseidon_hash_many;
-use starknet_ff::FieldElement;
-use std::vec;
-use tokio_stream::StreamExt;
-use torii_client::client::Client;
-use torii_grpc::client::EntityUpdateStreaming;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
-#[tokio::main]
-async fn main() {
-    println!("Running a rust torii client!");
+#[derive(Component)]
+struct Camera;
 
-    // client configuration
-    let torii_url = "http://0.0.0.0:8080".to_string();
-    let rpc_url = "http://0.0.0.0:5050".to_string();
-    let relay_url = "/ip4/127.0.0.1/tcp/9090".to_string();
-    let world: FieldElement = FieldElement::from_hex_be(
-        "0xb4079627ebab1cd3cf9fd075dda1ad2454a7a448bf659591f259efa2519b18",
-    )
-    .unwrap();
+#[derive(Component, Debug)]
+struct Position {
+    x: u32,
+    y: u32,
+}
 
-    // create a new client
-    let client: Client = Client::new(torii_url, rpc_url, relay_url, world, None)
-        .await
-        .unwrap();
+#[derive(Component, Debug)]
+struct PreviousPosition {
+    x: u32,
+    y: u32,
+}
 
-    // let metadata = client.metadata();
-    // let schema = metadata.model("Position").unwrap().schema.clone();
-    // println!("Metadata: {:?}", schema);
+#[derive(Component, Debug)]
+struct Moves {
+    remaining: u8,
+    last_direction: (bool, String),
+}
 
-    let mut stream = get_entities_stream(&client).await;
+fn main() {
+    App::new()
+        .add_systems(Startup, (setup_camera, spawn_player))
+        .add_systems(Update, (update_position, print_position))
+        .add_plugins(DefaultPlugins)
+        .run();
+}
 
-    while let Some(data) = stream.next().await {
-        match data {
-            Ok(data) => {
-                if data.models.is_empty() {
-                    println!("Skipping initialization response.");
-                } else {
-                    get_current_position(&data);
-                }
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let circle = Mesh2dHandle(meshes.add(Circle { radius: 25.0 }));
+    let color = Color::hsl(360. as f32 / 1. as f32, 0.95, 0.7);
+
+    commands.spawn((
+        Position { x: 0, y: 0 },
+        PreviousPosition { x: 0, y: 0 },
+        Moves {
+            remaining: 100,
+            last_direction: (true, "Up".to_string()),
+        },
+        MaterialMesh2dBundle {
+            mesh: circle,
+            material: materials.add(color),
+            ..default()
+        },
+    ));
+}
+
+fn update_position(
+    mut query: Query<(
+        &mut Position,
+        &mut PreviousPosition,
+        &mut Moves,
+        &mut Transform,
+    )>,
+) {
+    for (mut position, mut previous_position, mut moves, mut transform) in query.iter_mut() {
+        previous_position.x = position.x;
+        previous_position.y = position.y;
+
+        if moves.remaining != 0 {
+            if moves.last_direction.0 {
+                position.x += 1;
+                // position.y += 1;
+            } else {
+                position.x -= 1;
+                // position.y -= 1;
             }
-            Err(e) => {
-                println!("Error: {:?}", e);
-            }
+
+            transform.translation.x = position.x as f32 * 3.;
+            // transform.translation.y = position.y as f32 * 3.;
+            moves.remaining -= 1;
+        } else {
+            moves.remaining = 100;
+            moves.last_direction.0 = !moves.last_direction.0
         }
     }
 }
 
-async fn get_entities_stream(client: &Client) -> EntityUpdateStreaming {
-    // create hash of all models' keys (in this case, just one: the player's contract addresss)
-    let player_contract_address = FieldElement::from_hex_be(
-        "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
-    )
-    .unwrap();
-    let vec_keys = vec![player_contract_address.clone()];
-    let hashed_keys = poseidon_hash_many(&vec_keys);
-
-    // subscribe to the player's contract address
-    let stream = client.on_entity_updated(vec![hashed_keys]).await.unwrap();
-
-    stream
+fn print_position(query: Query<(Entity, &Position, &PreviousPosition, &Moves)>) {
+    for (entity, position, previous_position, moves) in query.iter() {
+        if previous_position.x != position.x {
+            info!(
+                "Entity: {:?}, Position: {:?}, Direction: {:?}",
+                entity, position, moves.last_direction.1
+            );
+        }
+    }
 }
 
-// todo: find what the event_key represents
-async fn _get_events_stream(client: &Client) -> EntityUpdateStreaming {
-    // create hash of all models' keys (in this case, just one: the player's contract addresss)
-    let event_key = FieldElement::from_hex_be(
-        "0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca",
-    )
-    .unwrap();
-    let vec_keys = vec![event_key.clone()];
-    let hashed_keys = poseidon_hash_many(&vec_keys);
-
-    // subscribe to the player's contract address
-    let stream = client
-        .on_event_message_updated(vec![hashed_keys])
-        .await
-        .unwrap();
-
-    stream
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((Camera2dBundle::default(), Camera));
 }
